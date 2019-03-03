7d79a0c8f7d8567799ae15962f21da28a51b3435
annasylvester <annaleesylvester@gmail.com>
2019-03-02 19:56:42 -0500

    Adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,39 @@
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password=1",
-  database: "taskmanager_db"
-});
+// let connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password=1",
+//   database: "taskmanager_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'password=1',
+    database: 'taskmanager_db'
+  })
+}
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
 module.exports = connection;
